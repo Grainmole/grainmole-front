2654763d172ae9f98f719181d45ea9d90fd98af4
Yaryna Kos <117029435+yarynakos@users.noreply.github.com>
2023-11-28 12:25:12 +0200

    update storage.service.ts urls

--- src/app/shared/services/storage/storage.service.ts
+++ src/app/shared/services/storage/storage.service.ts
@@ -12,13 +12,13 @@ export class StorageService {
   constructor(private http: HttpClient) { }
 
   public getAllUserStorages(): Observable<Storage[]> {
-    return this.http.get<Storage[]>('http://localhost:8080/users/storages', {headers: {
+    return this.http.get<Storage[]>('http://54.152.195.230:8080/users/storages', {headers: {
       'Authorization': `Bearer ${localStorage.getItem('access_token')}`
     }});
   }
 
   public addStorage(storage: StorageRequest): Observable<Storage> {
-    return this.http.post<Storage>('http://localhost:8080/storages', JSON.stringify(storage), {headers: {
+    return this.http.post<Storage>('http://54.152.195.230:8080/storages', JSON.stringify(storage), {headers: {
       'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
       'Content-Type': 'application/json'
     }});
